fd008a45123797837488c90680c8f75b331541fb
ignaciolembo7 <ignacio.lembo@ib.edu.ar>
2023-09-12 17:12:00 -0300

    update

--- Practica_2/p1_v3.py
+++ Practica_2/p1_v3.py
@@ -7,12 +7,14 @@ import seaborn as sns
 sns.axes_style("whitegrid")
 sns.set_style("ticks")
 
-# Valores iniciales: y = [s1, s2, n1, n2, m1, m2, h1, h2, V1, V2]
-#y_initial = np.array([s0(Vk), s0(Vk), n0(Vk), n0(Vk), m0(Vk), m0(Vk), h0(Vk)-1, h0(Vk)-1, Vk-1, Vk-1])
-y_initial = np.array([hh.s0(hh.Vk), hh.s0(hh.Vk)/2, hh.n0(hh.Vk), hh.n0(hh.Vk)/2, hh.m0(hh.Vk), hh.m0(hh.Vk)/2, hh.h0(hh.Vk), hh.h0(hh.Vk)/2, hh.Vk, hh.Vk/2])
-t_initial = 0.0
-t_final = 200
-time_step = 0.01
+#Parámetros de la simulación: y = [s1, s2, n1, n2, m1, m2, h1, h2, V1, V2]
+y_initial = np.array([hh.s0(hh.Vk), 0, hh.n0(hh.Vk), 0, hh.m0(hh.Vk), 0, hh.h0(hh.Vk), 0, hh.Vk, 0])
+t_initial = 0.0 #ms
+t_final = 500 #ms
+time_step = 0.01 #ms
+
+hh.gsyn = 1 #mS/cm2
+hh.Vsyn = 0 #mV
 
 #Simulacion
 steps = int(t_final/time_step) 
@@ -22,8 +24,6 @@ V2_values = np.empty(steps)
 
 t = t_initial
 y = y_initial
-hh.gsyn = 1
-hh.Vsyn = -80
 
 for i in range(steps):
     # Aplicar un paso de Runge-Kutta de orden 4
@@ -38,21 +38,21 @@ for i in range(steps):
 #Ploteo
 plt.plot(t_values, V1_values, color='darkorange',  label = "Neurona 1")
 plt.plot(t_values, V2_values, color='royalblue', label = "Neurona 2")
-plt.xlabel("Tiempo (ms)", fontsize=15,)
-plt.ylabel("Voltaje (mV)", fontsize=15,)
-# Adjust x-axis tick appearance
+plt.xlabel("Tiempo (ms)", fontsize=15)
+plt.ylabel("Voltaje (mV)", fontsize=15)
 plt.xticks(rotation=0, fontsize=15, color='black')
-# Adjust y-axis tick appearance
 plt.yticks(fontsize=15, color='black')
 plt.tick_params(direction='in', top=True, right=True, left=True, bottom=True)
 plt.legend(fontsize=15, framealpha=1, loc = 'upper right')
-plt.xlim(0, 120)  # Establecer límites en el eje x
-plt.grid(True, linewidth=0.5, linestyle='-', alpha=0.9)  # Establecer propiedades de la cuadrícula
-# Agregar texto en la esquina superior izquierda
-#plt.text(0.1, 0.95, '$g_{syn}$ = ' + str(hh.gsyn) + ' mS/cm$^2$ \n$V_{syn} = $' + str(hh.Vsyn) + ' mV', transform=plt.gca().transAxes,fontsize=12, verticalalignment='top', bbox=dict(facecolor='white', alpha=1))
-plt.annotate('$g_{syn}$ = ' + str(hh.gsyn) + ' mS/cm$^2$ \n$V_{syn} = $' + str(hh.Vsyn) + ' mV', xy=(0.1, 0.825), xycoords='axes fraction', fontsize=12, bbox=dict(boxstyle='round,pad=0.2', edgecolor='grey', facecolor='white'))
+plt.xlim(0, 120)
+plt.grid(True, linewidth=0.5, linestyle='-', alpha=0.9)
+plt.annotate('$g_{syn}$ = ' + str(hh.gsyn) + ' mS/cm$^2$ \n$V_{syn} = $' + str(hh.Vsyn) + ' mV', xy=(0.1, 0.85), xycoords='axes fraction', fontsize=12, bbox=dict(boxstyle='round,pad=0.2', edgecolor='grey', facecolor='white'))
 
 plt.savefig(f"../Redes-Neuronales/Practica_2/resultados/V_vs_t_{hh.gsyn}_{hh.Vsyn}.pdf")
 plt.savefig(f"../Redes-Neuronales/Practica_2/resultados/V_vs_t_{hh.gsyn}_{hh.Vsyn}.png", dpi=600)
 
 plt.show() 
+
+
+# Agregar texto en la esquina superior izquierda
+#plt.text(0.1, 0.95, '$g_{syn}$ = ' + str(hh.gsyn) + ' mS/cm$^2$ \n$V_{syn} = $' + str(hh.Vsyn) + ' mV', transform=plt.gca().transAxes,fontsize=12, verticalalignment='top', bbox=dict(facecolor='white', alpha=1))
--- Practica_2/p1_v4.py
+++ Practica_2/p1_v4.py
@@ -11,8 +11,8 @@ sns.set_style("ticks")
 fig1, ax1 = plt.subplots(figsize=(8,6)) 
 fig2, ax2 = plt.subplots(figsize=(8,6)) 
 
-for Vsyn in [0, -80]:
-    gsyns = np.linspace(0, 1, 20)
+for Vsyn in [0, -80]: #mV
+    gsyns = np.linspace(0, 1, 20) #mS/cm2
     f = []
     shift = []
     hh.Vsyn = Vsyn
@@ -21,10 +21,10 @@ for Vsyn in [0, -80]:
         hh.gsyn = gsyn
         print(hh.gsyn)
         # Valores iniciales: y = [s1, s2, n1, n2, m1, m2, h1, h2, V1, V2]
-        y_initial = np.array([hh.s0(hh.Vk), hh.s0(hh.Vk), hh.n0(hh.Vk), hh.n0(hh.Vk), hh.m0(hh.Vk), hh.m0(hh.Vk), hh.h0(hh.Vk), hh.h0(hh.Vk), hh.Vk, 0])
-        t_initial = 0.0
-        t_final = 1500
-        time_step = 0.01
+        y_initial = np.array([hh.s0(hh.Vk), 0, hh.n0(hh.Vk), 0, hh.m0(hh.Vk), 0, hh.h0(hh.Vk), 0, hh.Vk, 0])
+        t_initial = 0.0 #ms
+        t_final = 1500 #ms
+        time_step = 0.01 #ms
 
         #Simulacion
         steps = int(t_final/time_step) 
@@ -61,16 +61,18 @@ for Vsyn in [0, -80]:
         shift.append((Tshift%T)/T * 2 * np.pi) 
 
     if(Vsyn == 0):
-        color = 'darkorange'
+        color = 'r'
     else:
-        color = 'royalblue'
+        color = 'g'
 
     # Ploteo para la figura 1
     ax1.plot(gsyns, f, "-o", color= color,  label = "$V_{syn}$ = " + str(Vsyn) )
-    ax1.set_xlabel("$g_{syn}$ (mS/cm$^2$)", fontsize=15,)
-    ax1.set_ylabel("Frecuencia (Hz)", fontsize=15,)
+    ax1.set_xlabel("$g_{syn}$ (mS/cm$^2$)", fontsize=15)
+    ax1.set_ylabel("Frecuencia (Hz)", fontsize=15)
     ax1.tick_params(direction='in', top=True, right=True, left=True, bottom=True)
     ax1.legend(fontsize=15, framealpha=1)
+    ax1.tick_params(axis='x',rotation=0, labelsize=18, color='black')
+    ax1.tick_params(axis='y', labelsize=18, color='black')
     #ax1.set_xlim(0, 200)
     ax1.grid(True, linewidth=0.5, linestyle='-', alpha=0.9)
     #ax1.annotate('$g_{syn}$ = ' + str(hh.gsyn) + ' mS/cm$^2$ \n$V_{syn} = $' + str(hh.Vsyn) + ' mV', xy=(0.1, 0.825), xycoords='axes fraction', fontsize=12, bbox=dict(boxstyle='round,pad=0.2', edgecolor='grey', facecolor='white'))
@@ -81,19 +83,20 @@ for Vsyn in [0, -80]:
 
     # Ploteo para la figura 2
     ax2.plot(gsyns, shift, "-o", color= color, label = "$V_{syn}$ = " + str(Vsyn))
-    ax2.set_xlabel("$g_{syn}$ (mS/cm$^2$)", fontsize=15,)
-    ax2.set_ylabel("Desfasaje (rad)", fontsize=15,)
+    ax2.set_xlabel("$g_{syn}$ (mS/cm$^2$)", fontsize=15)
+    ax2.set_ylabel("Desfasaje (rad)", fontsize=15)
     ax2.tick_params(direction='in', top=True, right=True, left=True, bottom=True)
-    ax2.set_yticks([0, np.pi, 2*np.pi])
-    ax2.set_yticklabels(["$0$", r"$\pi$", r"$2\pi$"])
+    ax2.set_yticks([0, np.pi/2, np.pi])
+    ax2.set_yticklabels(["$0$",  r"$\frac{\pi}{2}$", r"$\pi$"])
     ax2.legend(fontsize=15, framealpha=1)
-    #ax2.set_xlim(0, 200)
+    ax2.tick_params(axis='x',rotation=0, labelsize=18, color='black')
+    ax2.tick_params(axis='y', labelsize=18, color='black')
     ax2.grid(True, linewidth=0.5, linestyle='-', alpha=0.9)
-    #ax2.annotate('$g_{syn}$ = ' + str(hh.gsyn) + ' mS/cm$^2$ \n$V_{syn} = $' + str(hh.Vsyn) + ' mV', xy=(0.1, 0.825), xycoords='axes fraction', fontsize=12, bbox=dict(boxstyle='round,pad=0.2', edgecolor='grey', facecolor='white'))
 
     # Guardar figura 2
     fig2.savefig(f"../Redes-Neuronales/Practica_2/resultados/desfasaje_vs_gsyn.pdf")
     fig2.savefig(f"../Redes-Neuronales/Practica_2/resultados/desfasaje_vs_gsyn.png", dpi=600)
    
 plt.show() 
-
+#ax2.annotate('$g_{syn}$ = ' + str(hh.gsyn) + ' mS/cm$^2$ \n$V_{syn} = $' + str(hh.Vsyn) + ' mV', xy=(0.1, 0.825), xycoords='axes fraction', fontsize=12, bbox=dict(boxstyle='round,pad=0.2', edgecolor='grey', facecolor='white'))
+#ax2.set_xlim(0, 200)
